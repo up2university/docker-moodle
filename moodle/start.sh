914c848e3f90db9830d1b6d09fd47a4946d55413
Allan Third <allan.third@open.ac.uk>
2017-04-12 16:38:51 +0100

    Remove SSHD from Moodle Docker

--- moodle/start.sh
+++ moodle/start.sh
@@ -4,15 +4,12 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
   /usr/bin/mysqld_safe &
   sleep 10s
   MOODLE_DB="moodle"
-  echo root:$SSH_PASSWORD | chpasswd
   sed -e "s/pgsql/mysqli/
   s/localhost/mysql/
   s/username/moodle/
   s/password/$MOODLE_PASSWORD/
   s/example.com/$VIRTUAL_HOST/
   s/\/home\/example\/moodledata/\/var\/moodledata/" /var/www/html/moodle/config-dist.php > /var/www/html/moodle/config.php
-
-  sed -i 's/PermitRootLogin without-password/PermitRootLogin Yes/' /etc/ssh/sshd_config
 
   chown www-data:www-data /var/www/html/moodle/config.php
 
