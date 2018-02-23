94890c3994992944b2ecb3c19eec4179a1ceac29
Allan Third <allan.third@open.ac.uk>
2018-02-23 11:07:53 +0000

    Episode V: the stupid typo strikes back

--- moodle/config/moosh-theme_fordson.sh
+++ moodle/config/moosh-theme_fordson.sh
@@ -1,7 +1,7 @@
 cp ../../../envs/UP2U_Moodle_400x200.png /
 cp ../../../envs/Moodle_Header_1500x600.jpg /
 chown www-data:www-data /UP2U_Moodle_400x200.png /Moodle_Header_1500x600.jpg
-if [ ! "$(/moosh/moosh.php -n file-list -i "filename='UP2U_Moodle_400x200.pnd' and filearea like 'logo%' and contextid=1")" = "" ]; then
+if [ ! "$(/moosh/moosh.php -n file-list -i "filename='UP2U_Moodle_400x200.png' and filearea like 'logo%' and contextid=1")" = "" ]; then
     /moosh/moosh.php -n file-list -i "filename='UP2U_Moodle_400x200.png' and filearea like 'logo%' and contextid=1" | /moosh/moosh.php -n file-delete -s
 fi
 
