be106a94ed603d30ec3889449c7244b9905af6e1
Allan Third <allan.third@open.ac.uk>
2018-03-15 11:04:47 +0000

    Fixed typo in fordson moosh file.

--- moodle/config/moosh-theme_fordson.sh
+++ moodle/config/moosh-theme_fordson.sh
@@ -5,6 +5,14 @@ if [ -f /moosh-theme_fordson-files.sh ]; then
     rm /moosh-theme_fordson-files.sh
 fi
 
+set -a
+source /envs/theme_fordson*.env
+set +a
+
+for mod in ${THEME_FORDSON_HIDE_MODS}; do 
+    /moosh/moosh.php -n module-manage hide ${mod}
+done
+
 for variable in $(grep 'png\|jpg' ../../../envs/theme_fordson.env | cut -f 1 -d '=') ; do
     set -a
     filename=$(grep ${variable}= ../../../envs/theme_fordson.env | cut -f 2 -d '=' | sed 's%\\/%%g' | sed 's%\\\"%%g' | sed 's%/%%g')
