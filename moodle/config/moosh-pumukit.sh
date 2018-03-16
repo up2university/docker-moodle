9245f0510bb2c28f5fa79935e12ceede46b31f50
Allan Third <allan.third@open.ac.uk>
2018-03-16 12:24:51 +0000

    Enabled PuMuKit filters.

--- moodle/config/moosh-pumukit.sh
+++ moodle/config/moosh-pumukit.sh
@@ -3,3 +3,5 @@ source /envs/pumukit*.env
 set +a
 
 /moosh/moosh.php -n config-set toolbar "$(echo -e ${PUMUKIT_EDITOR_ATTO_TOOLBAR})" editor_atto
+/moosh/moosh.php -n filter-set pumukit 1
+/moosh/moosh.php -n filter-set pumukitpr 1
