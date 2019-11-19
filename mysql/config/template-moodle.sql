d85d85220e66d9a19df38e113a8d761e6752d67b
Pasquale Iero <pasquale.iero@open.ac.uk>
2019-11-19 14:04:14 +0000

    Fix for issue 165

---
+++ mysql/config/template-moodle.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 1, allowassign = 11;
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 2, allowassign = 11;
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 3, allowassign = 11;
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 4, allowassign = 11;
