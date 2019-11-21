c551a6053cbb66e69a64953d7c611d5c408c3c65
Pasquale Iero <pasquale.iero@open.ac.uk>
2019-11-21 09:36:37 +0000

    Improving settings

--- mysql/config/template-moodle.sql
+++ mysql/config/template-moodle.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 1, allowassign = 11;
-INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 2, allowassign = 11;
-INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 3, allowassign = 11;
-INSERT IGNORE INTO mdl_role_allow_assign SET roleid = 4, allowassign = 11;
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = (SELECT id FROM mdl_role WHERE shortname= 'manager'), allowassign = (SELECT id FROM mdl_role WHERE shortname= 'editingstudent');
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = (SELECT id FROM mdl_role WHERE shortname= 'coursecreator'), allowassign = (SELECT id FROM mdl_role WHERE shortname= 'editingstudent');
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = (SELECT id FROM mdl_role WHERE shortname= 'editingteacher'), allowassign = (SELECT id FROM mdl_role WHERE shortname= 'editingstudent');
+INSERT IGNORE INTO mdl_role_allow_assign SET roleid = (SELECT id FROM mdl_role WHERE shortname= 'teacher'), allowassign = (SELECT id FROM mdl_role WHERE shortname= 'editingstudent');
